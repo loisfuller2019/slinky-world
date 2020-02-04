aa7bc9b56f2b2fc71f37984550322f5933d6bd36
Lois Fuller <57360970+loisfuller2019@users.noreply.github.com>
2020-02-04 20:20:44 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,128 @@
+function setup() {
+  createCanvas(500, 500, WEBGL);
+}
+
+
+
+function draw() {
+  
+  
+ 
+  background('#0000');
+  rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(100, 5);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+   rotateY(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(200, 2);
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+   rotateY(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+   rotateX(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  torus(200, 2);
+  
+  
+}
